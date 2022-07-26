f8077d6c82196b479456cef8e19f6d0895e0505d
Faizan Ahmad <faizan13801@gmail.com>
2022-07-26 19:13:24 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Sample extends React.Component {
+    constructor(props) {
+      super(props);
+      // to bind the method with event handler without (). 
+      this.handleChange = this.handleChange.bind(this);
+      this.state =
+      { 
+          value: ''
+      };
+  }
+  handleChange(e){
+    this.setState({value: e.target.value});
+  }
+  render(){
+    return <>
+        <div className="container">
+        <div className="input">
+            <h3>Input</h3>
+            <textarea className="input-text" onChange={this.handleChange} defaultValue={this.state.value}/>
+        </div>
+        <div className="output">
+            <h3>Pro Note</h3>
+            <div className="output-text">{this.state.value}</div>
+        </div>                
+        </div>
+    </>
+  }
 }
-
-export default App;
+export {Sample};
